bbc79b2836729c8c97ae8243811742b9ea4c3935
Mor1000 <33144215+Mor1000@users.noreply.github.com>
2018-12-13 18:51:09 +0200

    new encoder function1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_1_gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_1_gyro.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -13,12 +14,28 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 public class Red_1_gyro extends LinearOpMode {
     Robot robot;
     DcMotor[][] DriveMotors;
+    public double[] GyroPID(double heading, double lasterror, BNO055IMU imu) {
+        double kp = 0.015, kd = 0.01, ki = 0, nexterror = 0;
+        double err = heading - imu.getAngularOrientation(AxesReference.INTRINSIC,
+                AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        while (err > 180)
+            err = err - 360;
+        while (err < -180)
+            err = err + 360;
+        lasterror = err - lasterror;
+        double pd = nexterror * ki + lasterror * kd + err * kp;
+        return (new double[]{-pd, err});
+    }
 
     public void gyro_drive() {
         double power = 0.3;
+        double pidErr[] = {0, 0};
+
         setMotorPower(new double[][]{{power, power}, {power, power}});
         while (opModeIsActive() && getYaw() >= -14.3) {
             setMotorPower(new double[][]{{power, power}, {power, power}});
+            pidErr = GyroPID(getheading(), pidErr[1], robot.imu);
+            setMotorPower(new double[][]{{power - pidErr[0], power + pidErr[0]}, {power - pidErr[0], power + pidErr[0]}});
         }
         setMotorPower(new double[][]{{0, 0}, {0, 0}});
 
@@ -37,6 +54,10 @@ public class Red_1_gyro extends LinearOpMode {
 
     public float getYaw() {
         return robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).thirdAngle;
+    }
+    public float getheading(){
+        return robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).secondAngle;
+
     }
 
     public void setMotorPower(double[][] power) { //Stores the four drivetrain motors power in array
