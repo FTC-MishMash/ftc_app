f7743db3ec2d58ed580011f21b5dc830172496e0
Mor1000 <33144215+Mor1000@users.noreply.github.com>
2018-12-30 21:26:08 +0200

    ariel landing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LandInAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LandInAuto.java
@@ -14,18 +14,23 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 @Autonomous
 public class LandInAuto extends LinearOpMode {
     Robot robot;
-    autoMode auto;
 
 //
     public void LandInAuto() {
+        telemetry.addData("pitch",getAngularOriention().thirdAngle);
+        telemetry.update();
         robot.linear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.linear.setTargetPosition(0);
+        robot.linear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.linear.setPower(1);
+        robot.shaft[0].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.shaft[1].setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.shaft[0].setPower(0.5);
+        robot.shaft[1].setPower(0.5);
         while (opModeIsActive() && getAngularOriention().thirdAngle <= -5) {
-            robot.linear.setTargetPosition(0);
-            robot.linear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.linear.setPower(1);
-
-            robot.shaft[0].setPower(0.3);
-            robot.shaft[1].setPower(0.3);
+            telemetry.addData("pitch",getAngularOriention().thirdAngle);
+            telemetry.addData("linear encoder",robot.linear.getCurrentPosition());
+            telemetry.update();
         }
         robot.shaft[0].setPower(0);
         robot.shaft[1].setPower(0);
@@ -42,27 +47,56 @@ public class LandInAuto extends LinearOpMode {
         robot.shaft[0].setPower(1);
         robot.shaft[1].setPower(1);
 
-        while (opModeIsActive() && robot.linear.getCurrentPosition() >= 1180 && robot.linear.getCurrentPosition() <= 1200) {
-            robot.linear.setTargetPosition(1200);
-            robot.linear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.linear.setPower(1);
-        }
+        robot.linear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.linear.setTargetPosition(1200);
+        robot.linear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.linear.setPower(1);
+        while (opModeIsActive() && robot.linear.getCurrentPosition() <= 1180 );
+
+        robot.linear.setPower(0);
+        robot.shaft[0].setPower(0);
+        robot.shaft[1].setPower(0);
+
         double t0 = getRuntime();
-robot.linear.setPower(0);
-auto.setMotorPower(new double[][]{{0.3,0.3},{0.3,0.3}});
+        setMotorPower(new double[][]{{0.3,0.3},{0.3,0.3}});
 
         while (opModeIsActive()&& getRuntime()-t0 <= 0.5 );
-        auto.setMotorPower(new double[][]{{0,0},{0,0}});
+        setMotorPower(new double[][]{{0,0},{0,0}});
 
-        robot.shaft[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        robot.shaft[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        robot.linear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        robot.shaft[0].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        robot.shaft[1].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        robot.linear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
     }
-
+    public void setMotorPower(double[][] power) { //Stores the four drivetrain motors power in array
+        for (int row = 0; opModeIsActive() && row < 2; row++)
+            for (int col = 0; opModeIsActive() && col < 2; col++)
+                robot.driveTrain[row][col].setPower(power[row][col]);
+    }
     @Override
+
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap);
+        while (!isStarted()){
+            telemetry.addData("pitch",getAngularOriention().thirdAngle);
+            telemetry.update();
+            robot.linear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.shaft[0].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.shaft[1].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.linear.setTargetPosition(0);
+            robot.shaft[0].setTargetPosition(0);
+            robot.shaft[1].setTargetPosition(0);
+
+            robot.linear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.shaft[0].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.shaft[1].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            robot.linear.setPower(1);
+            robot.shaft[0].setPower(1);
+            robot.shaft[1].setPower(1);
+        }
         waitForStart();
         LandInAuto();
 
