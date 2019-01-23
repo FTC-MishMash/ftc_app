286c4d83166faa75c836744c3edd69c3b84f50a1
InbarKedem <Inbar12103>
2019-01-23 20:20:51 +0200

    ariel drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkingTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name = "ParkingTest")
+
+public class ParkingTest extends LinearOpMode  {
+    Robot robot;
+    AutoMode auto;
+    public void Parking(int targetPositionEncoder) {
+        robot.shaft[0].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.shaft[1].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.shaft[0].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.shaft[1].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        auto.driveByEncoderRoverRuckus(90, 90, -0.5);
+        robot.shaft[0].setTargetPosition(targetPositionEncoder);//250
+        robot.shaft[1].setTargetPosition(targetPositionEncoder);
+
+        robot.shaft[0].setPower(0.6);
+        robot.shaft[1].setPower(0.6);
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+     waitForStart();
+     Parking(3000);
+    }
+}
