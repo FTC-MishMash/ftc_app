c047bfe9b0e10ff974d3e2dd4b3cefc080ddcd5c
InbarKedem <Inbar12103>
2019-01-24 16:02:30 +0200

    ariel drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkingTest.java
@@ -4,29 +4,33 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous (name = "ParkingTest")
+@Autonomous(name = "ParkingTest")
 
-public class ParkingTest extends LinearOpMode  {
+public class ParkingTest extends AutoMode {
 
     Robot robot;
-    AutoMode auto;
+    DriveUtilities ds;
+
     public void Parking(int targetPositionEncoder) {
         robot.shaft[0].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.shaft[1].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.shaft[0].setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.shaft[1].setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        auto.driveByEncoderRoverRuckus(90, 90, -0.5);
+        ds.driveByEncoderRoverRuckus(90, 90, 0.5, false);
         robot.shaft[0].setTargetPosition(targetPositionEncoder);//250
         robot.shaft[1].setTargetPosition(targetPositionEncoder);
 
         robot.shaft[0].setPower(0.6);
         robot.shaft[1].setPower(0.6);
     }
+
     @Override
 
     public void runOpMode() throws InterruptedException {
-     waitForStart();
-     Parking(3000);
+        robot = new Robot(hardwareMap);
+
+        waitForStart();
+        Parking(3000);
 
     }
 }
