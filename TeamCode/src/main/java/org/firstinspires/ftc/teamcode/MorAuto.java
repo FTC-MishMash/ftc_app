eac22a74eccf06193c9b305d5918d6aa18c37b72
InbarKedem <Inbar12103>
2019-02-10 20:37:30 +0200

    Push MorAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MorAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MorAuto.java
@@ -5,11 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 @Autonomous(name = "AutoWithoutLanding")
 public class MorAuto extends AutoMode {
+
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
-//        auto = new autoMode();
-        // robot = new Robot(hardwareMap);
+
         if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
             tsSampling.initTfod();
         } else {
@@ -17,70 +17,75 @@ public class MorAuto extends AutoMode {
             telemetry.update();
         }
 //        robot.hanging.setPosition(robot.hangingLockPosition);
+        telemetry.addLine("wait for start");
+        telemetry.update();
         waitForStart();
+
 
         if (opModeIsActive()) {
             runTime.reset();
             runTime.startTime();
-
+//        getOffTheClimb
+//            driveUtils.TurnWithEncoder(50, 0.6);
+//            sleep(1500);
+//            driveUtils.TurnWithEncoder(0, 0.6);
+//            sleep(1000);
             int cubePosition = 0;
-            cubePosition = tsSampling.searchCube(0.35, robot.SamplingAngleRight, robot.SamplingAngleLeft);
+            cubePosition = tsSampling.searchCube(0.33, robot.SamplingAngleRight, robot.SamplingAngleLeft);
 
             telemetry.addData("Gold mineral position: ", cubePosition);
             telemetry.update();
-            // driveUtils.driveByEncoderRoverRuckus(20, 40, 0.35, false);
-
-            telemetry.addLine("finished go to cube");
-            telemetry.update();
-//            sleep(1500);
+            if (tfod != null)
+                tfod.activate();
+//            sleep(800);
             driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingForward, robot.driveEncoderSamplingForward, robot.powerEncoder, false);
-            if (cubePosition!=1)
-            {
+            if (cubePosition != 1) {
                 driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingPositionSide, robot.driveEncoderSamplingPositionSide, robot.powerEncoder, false);
-                driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingPositionSideBackward,robot.driveEncoderSamplingPositionSideBackward, -robot.powerEncoder, false);
-            }
-            else
-            {
+                driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingPositionSideBackward, robot.driveEncoderSamplingPositionSideBackward, -robot.powerEncoder, false);
+            } else {
                 driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingPositionMiddle, robot.driveEncoderSamplingPositionMiddle, robot.powerEncoder, false);
                 driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingPositionMiddleBackward, robot.driveEncoderSamplingPositionMiddleBackward, -robot.powerEncoder, false);
             }
+            if (tfod != null) {
+                tfod.shutdown();
+            }
+            vuforia.close();
             telemetry.addLine("finished driving into cube");
             telemetry.update();
-//            sleep(2500);
+//            sleep(500);
             driveUtils.driveByEncoderRoverRuckus(robot.driveEncoderSamplingBackward, robot.driveEncoderSamplingBackward, -robot.powerEncoder, false);
             telemetry.addLine("finished driving out of cube");
-
-
             telemetry.update();
+            sleep(200);
+//
+            //  driveUtils.setMotorPower(robot.driveTrain, new double[][]{{0, 0}, {0, 0}});
+
+
+            driveUtils.TurnWithEncoder(robot.angleTurnToImage, 0.5);
+            sleep(100);
+            tsSampling.initVuforiaWebCam(false);
+            targetNav.startTracking();
 //            sleep(500);
 
+            float[] pos = targetNav.getPositions();
+            telemetry.addData("pos null: ", pos == null);
+            telemetry.update();
+//            sleep(500);
+            if (pos == null) {
 
+                telemetry.addData("start searching wait for click", cubePosition);
+                telemetry.update();
+//                sleep(500);
+                targetNav.searchImage(cubePosition, -0.20);
+            }
 
-
-            driveUtils.TurnWithEncoder(robot.newAngleTurnToImage, 0.5);
-//            sleep(2500);
-
-
-            //driveUtils.back_up_driveByImage(0.45, robot.AngleToDepot, -(30 + cubePosition * 15));
-            driveUtils.back_up_driveByImage(0.45, robot.newAngleToDepot, 95);
-            //driveUtils.driveByEncoderRoverRuckus(-80, -80, -0.4, false);//to depot
+//            sleep(600);
+            targetNav.driveToImage(-0.3);
+//            sleep(500);
             driveUtils.driveByEncoderRoverRuckus(robot.distToDepot, robot.distToDepot, robot.powerEncoder, false);//to depot
             Marker(0.5,robot.shaftTargetPositionMarker);  //marker
             // driveUtils.driveByEncoderRoverRuckus(90, 90, -0.5, false);//to crater
-            driveUtils.driveByEncoderRoverRuckus(-robot.distToCrater/2, -robot.distToCrater/2, -robot.powerEncoder, false);//to crater
-            driveUtils.TurnWithEncoder(45, 0.5);
-            driveUtils.TurnWithEncoder(-45, 0.5);
-            driveUtils.driveByEncoderRoverRuckus(robot.distToCrater/2, robot.distToCrater/2, robot.powerEncoder, false);//to crater
-            Parking(230,-3300,-700,0.3);
-            //TODO: to change linear target encoder!!
-//            driveByColor(0,robot.colorRightFront,robot.imu,robot.hsvValuesRightFront,AngleToDepot,0.35);
-//            }
-//            sleep(1000);
-            //       driveUtils.driveByEncoderRoverRuckus(60, 60, 0.5,false);
-
-            //go to crater
-            //open shaft
-
+            driveUtils.driveByEncoderRoverRuckus(robot.distToCrater, robot.distToCrater, -robot.powerEncoder, false);//to crater
         }
     }
 }
